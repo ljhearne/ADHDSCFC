9e044b2f0a752d21f58c996b1cfad4ab003d733a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-02-12 15:02:19 +1000

    Accommodate special case of two values of sigma

--- Scripts/Model/perform_pse_subjects.m
+++ Scripts/Model/perform_pse_subjects.m
@@ -1,4 +1,4 @@
-function [aFCPCC, c_best, c_critic, corr_max, c_range, correl, std_sigmas] = perform_pse_subjects(SC, FCPCC, sigma_noise, HBN)
+function [aFCPCC, c_best, c_critic, corr_max, c_range, correl, std_sigmas] = perform_pse_subjects(SC, FCPCC, sigma_noise, HBN, mu_b)
 
 % This is a dummy function to wrap the parfor and the parameter exploration
 % to detect the optimal c-value across subjects
@@ -13,11 +13,16 @@ function [aFCPCC, c_best, c_critic, corr_max, c_range, correl, std_sigmas] = per
     if nargin < 4 || isempty(HBN)
         % Hack to make this function work for all cases
         node_attributes = ones(number_of_subjects, number_of_nodes);
+        
     else
-        node_attributes = HBN; 
+        node_attributes = HBN;
     end
     
-    
+    if nargin < 5
+        % This value of mu is used for the nodes that do not have the
+        % attribute specified in HBN
+        mu_b = 1;
+    end
     
 
     prec = 0.05;     % Percentage below c_critic at which the range to explore will be truncated
@@ -40,7 +45,7 @@ function [aFCPCC, c_best, c_critic, corr_max, c_range, correl, std_sigmas] = per
     parfor subject_idx = 1:size(SC, 3)
         empSC   = SC(:, :, subject_idx);
         empPCC  = FCPCC(:, :, subject_idx);
-        [Sigma(:, :, subject_idx), std_sigmas(subject_idx)] = introduce_node_based_variability(sigma_noise, node_attributes(subject_idx, :));
+        [Sigma(:, :, subject_idx), std_sigmas(subject_idx)] = introduce_node_based_variability(sigma_noise, node_attributes(subject_idx, :), mu_b);
         
         [aFCPCC(:, :, subject_idx), c_best(subject_idx), ...     % optimal value of coupling that maximizes similarity between empFC and anaFC
             c_critic(subject_idx), ...   % critical value of coupling derived from empSC
