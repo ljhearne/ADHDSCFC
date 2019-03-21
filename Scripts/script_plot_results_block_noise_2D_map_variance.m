d2b259579f3c1b021916522385f4e69a4f6f7e8e
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-03-21 10:10:09 +1000

    Update plotting script that reproduces Fig 3D-E in the paper

--- Scripts/script_plot_results_block_noise_2D_map_variance.m
+++ Scripts/script_plot_results_block_noise_2D_map_variance.m
@@ -1,15 +1,11 @@
-% Load modelling results
-load('./Results/Schaeffer214-Model/CTRL_block_var_noise_16x16_evals_adhd.mat')
+% Plot 2D var maps for all connections
+load('./Results/Schaeffer214-Model/CTRL_block_var_noise_16x16_hub_periphery_10_percent.mat')
 
-
-figure_handle = figure;
-
-% 
+figure_handle = figure; 
 for kk=1:4
     ax(kk) = subplot(1, 4, kk);
     hold(ax(kk), 'on')
 end
-
 
 % Get axis data and square to get variance info
 % Variance in the hubs
@@ -29,48 +25,40 @@ r(3).map = reshape(median(r_ctrl_esc_afc.feed_edges), size(VSH));
 % Periphery
 r(4).map = reshape(median(r_ctrl_esc_afc.periphery_edges), size(VSH));
 
-min_r_val = [];
-max_r_val = [];
-
+cmap = parula(65);
 for kk=1:4
-    min_r_val = [min_r_val min(r(kk).map)];
-    max_r_val = [max_r_val max(r(kk).map)];
-end
+    dummy = imgaussfilt(r(kk).map, 1);
+    ih(kk) = imagesc(ax(kk), vsh, vsp, dummy);
 
-min_r_val = min(min_r_val);
-max_r_val = max(max_r_val);
-
-
-for kk=1:4
-    ih(kk) = imagesc(ax(kk), vsh, vsp, r(kk).map);
     set(ax(kk), 'YDir', 'Normal')
     ax(kk).XLim = [min(vsh(:)) max(vsh(:))];
     ax(kk).YLim = [min(vsp(:)) max(vsp(:))];
-    caxis(ax(kk), [0.2 0.95])
+    caxis(ax(kk), [0.15 1])
     axis(ax(kk), 'square')
     ax(kk).Title.String = title_str{kk};
     ax(kk).XLabel.String = 'Var[\sigma_H]';
     ax(kk).YLabel.String = 'Var[\sigma_P]';
     ch(kk) = colorbar(ax(kk));
     ch(kk).Location = 'SouthOutside';
-    ch(kk).LineWidth = 1.5;    
+    ch(kk).LineWidth = 1.5;
+    ax(kk).Colormap = cmap;
 end
 
 % Plot identity line
-line_color = {'k', 'w', 'k', 'w'};
+line_color = {'k', 'k', 'k', 'k'};
 for kk=1:4
     plot(ax(kk), vsh, vsp, 'color', line_color{kk}, 'linestyle', '-')
 end
 
-%% Plot the results for feeder locations
+%% Plot the results for feeder connections
 figure_handle_feeder = figure;
 ax_f = subplot(1,1,1);
 
-ih(5) = imagesc(ax_f, vsh, vsp, r(3).map);
+ih(5) = imagesc(ax_f, vsh, vsp, imgaussfilt(r(3).map, 1));
 set(ax_f, 'YDir', 'Normal')
 ax_f.XLim = [min(vsh(:)) max(vsh(:))];
 ax_f.YLim = [min(vsp(:)) max(vsp(:))];
-caxis(ax_f, [0.2 0.95])
+caxis(ax_f, [0.15 1])
 ax_f.Title.String = title_str{3};
 ax_f.XLabel.String = 'Var[\sigma_H]';
 ax_f.YLabel.String = 'Var[\sigma_P]';
