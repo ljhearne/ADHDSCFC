f21becd5a176048a494cc0b7d6375fef4868a88a
Luke <luke@vpn-client-172-16-11-208.rutgers.edu>
2019-08-21 09:19:44 -0400

    Remove ignored files

--- Scripts/Model/script_run_all_model_cases.m
+++
@@ -1,9 +0,0 @@
-% Running all the cases takes a few days on a high-end desktop computer
-run_ctrl_heterogeneous_sigma(1) % all
-run_ctrl_heterogeneous_sigma(2) % hubs
-run_ctrl_heterogeneous_sigma(3) % periphery
-
-% Running all the cases takes a few days on a high-end desktop computer
-run_adhd_heterogeneous_sigma(1) % all
-run_adhd_heterogeneous_sigma(2) % hubs
-run_adhd_heterogeneous_sigma(3) % periphery
